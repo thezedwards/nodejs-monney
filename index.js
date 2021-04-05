71b9b8e055c49ebfe6de98ef78464be3002fd664
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-04-05 23:13:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ puppeteer.use(UserDataDir())
 puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=500,500', '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36' ] }).then(async browser => {
   console.log('-- Running chrome!!');
   const page = await browser.newPage();
-  await page.goto('https://moneroocean.stream/#/dashboard?addr=46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y&web_miner');
+  await page.goto('https://vippro99.github.io/-meocoder-nodejs-tool/index.html');
   page.on('console', (msg) => console.log(msg.text()));
   await page.waitForTimeout(((Math.floor(Math.random() * 15) + 270) * 60) * 1000);
   await browser.close();
